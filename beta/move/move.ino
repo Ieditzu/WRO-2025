0892043a4a0d9afaff99b0da58af7f1228717586
Ieditzu <126756320+Ieditzu@users.noreply.github.com>
2025-05-26 10:16:47 +0300

    v0.4 beta 2
    made a move code working on it. removed 2 unnecessarily codes.

---
+++ beta/move/move.ino
@@ -0,0 +1,65 @@
+#include <Servo.h>
+
+const int IN1 = 7;
+const int IN2 = 8;
+const int ENA = 9;
+
+const int SERVO_PIN = 10;
+Servo steeringServo;
+
+void setup() {
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(ENA, OUTPUT);
+
+  analogWrite(ENA, 204);
+
+  steeringServo.attach(SERVO_PIN);
+  center();
+
+  stop();
+}
+
+void loop() {
+  left();
+  delay(1000);
+
+  analogWrite(ENA, 255);
+
+  forward();
+  delay(2000);
+
+  right();
+  delay(1000);
+
+  stop();
+  center();
+  delay(2000);
+}
+
+void forward() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+}
+
+void backward() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+}
+
+void stop() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+}
+
+void left() {
+  steeringServo.write(60);
+}
+
+void right() {
+  steeringServo.write(120);
+}
+
+void center() {
+  steeringServo.write(90);
+}
--- main.py
+++
@@ -1,9 +0,0 @@
-import motor_control.py as motor
-
-motor.forward()
-time.sleep(1)
-motor.left()
-time.sleep(0.5)
-motor.stop()
-motor.cleanup()
-
